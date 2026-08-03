89b9163f341f098872db69450b7e2e024f903aa2
Subash Skanthakumar <142605816+subash913@users.noreply.github.com>
2026-08-02 21:17:25 -0500

    Add: car-fleet - submission-3

---
+++ Data Structures & Algorithms/car-fleet/submission-3.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        if (position.size() == 1) {
+            return 1;
+        }
+       int fleets = position.size();
+       vector<int> stack;
+       vector<pair<int,int>> ps;
+       for (int i = 0; i < position.size(); ++i) {
+        ps.push_back({position[i], speed[i]});
+       } 
+
+       sort(ps.begin(), ps.end());
+       double time = (double) (target - ps[ps.size()-1].first) / ps[ps.size()-1].second;
+       for (int i = ps.size() - 2; i > -1; --i) {
+        double currtime = (double) (target - ps[i].first) / ps[i].second;
+        if (currtime <= time) {
+            fleets--;
+        } else {
+            time = currtime;
+        }
+       }
+
+       return fleets;
+    }
+};
