a559b1265a51a39c36a862a63b646277e8454042
Subash Skanthakumar <142605816+subash913@users.noreply.github.com>
2026-08-12 22:35:44 -0500

    Add: combination-target-sum - submission-4

---
+++ Data Structures & Algorithms/combination-target-sum/submission-4.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> combos;
+    vector<int> integers;
+    int targ;
+
+    vector<vector<int>> combinationSum(vector<int>& nums, int target) {
+        integers = nums;
+        vector<int> empty;
+        targ = target;
+        sumup(empty, 0, 0);
+        return combos;
+
+    }
+
+    void sumup(vector<int>& combo, int index, int sum) {
+        if (sum == targ) {
+            combos.push_back(combo);
+            return;
+        }
+        if (sum > targ || index == integers.size()) {
+            return;
+        }
+        sumup(combo, index + 1, sum);
+        combo.push_back(integers[index]);
+        sum += integers[index];
+        sumup(combo, index, sum);
+        
+        combo.pop_back();
+
+
+    }
+};
