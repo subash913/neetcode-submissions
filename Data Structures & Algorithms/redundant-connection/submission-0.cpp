2c41f29a793e98074bf1c8893a4a7153bb63b21c
Subash Skanthakumar <142605816+subash913@users.noreply.github.com>
2026-09-15 18:53:08 -0500

    Add: redundant-connection - submission-0

---
+++ Data Structures & Algorithms/redundant-connection/submission-0.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        vector<int> parent(edges.size() + 1);
+        vector<int> size(edges.size() + 1,1);
+        vector<vector<int>> edges2;
+        for (int i = 0; i < edges.size(); ++i) {
+            parent[i] = i;
+        }
+        int components = edges.size();
+        for (int i = 0; i < edges.size(); ++i) {
+            if (!unite(edges[i][0], edges[i][1], parent, size, components)) {
+                edges2.push_back(edges[i]);
+            }
+        }
+        if (!edges2.empty()) {
+            return edges2[edges2.size() - 1];
+        } else {
+            return {-1};
+        }
+        
+    }
+
+    int find(int node, vector<int>& parent) {
+        if (node == parent[node]) {
+            return node;
+        }
+        parent[node] = find(parent[node], parent);
+        return parent[node];
+    }
+
+    bool unite(int node1, int node2, vector<int>& parent, vector<int>& size, int& components) {
+        int root1 = find(node1, parent);
+        int root2 = find(node2, parent);
+        
+        if (root1 == root2) {
+            return false;
+        }
+        if (size[root1] < size[root2]) {
+            swap(root1, root2);
+        }
+        parent[root2] = root1;
+        size[root1] += size[root2];
+        components--;
+        return true;
+    }
+};
